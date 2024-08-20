fe66171aea2655d2479d49c7711d6eecde3d3d4d
João Barbosa <contato.jsbarbosa@gmail.com>
2024-08-20 20:21:20 -0300

    Iniciando projeto

---
+++ backend/src/main/java/com/joaobarbosadev/WolfAcademy/WolfAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.joaobarbosadev.WolfAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolfAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WolfAcademyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/joaobarbosadev/WolfAcademy/WolfAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaobarbosadev.WolfAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WolfAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
